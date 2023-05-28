8b4d9ae34464b96c248474eacb1d2985f10d9a33
AidarSm <kaumakov@gmail.com>
2023-05-28 20:35:23 +0300

    addEventListener FlightSelection4

--- Version 2/js/homepage.js
+++ Version 2/js/homepage.js
@@ -10,7 +10,7 @@ wrapper.forEach(function (item) {
 
 // Навешиваем обработчик событий на элемент "icon-menu"
 iconMenu.forEach(function (item) {
-  item.addEventListener("click", function (event) {
+  item.addEventListener("click", function () {
     // Переключаем класс "active" у элемента "icon-menu"
     this.classList.toggle("active");
     menuBody.forEach(function (menu) {
@@ -57,67 +57,7 @@ askButton.addEventListener("click", function () {
     .catch((error) => {
       console.log(error);
     });
-  //  Добавляем в график период для просмотра динамики бронирования
-  let startDate = date3.value;
-  let endDate = date2.value;
-
-  function getDates(startDate, endDate) {
-    const dates = [];
-    let currentDate = new Date(startDate);
-    const endDateTime = new Date(endDate).getTime();
-
-    while (currentDate.getTime() <= endDateTime) {
-      let day = ("0" + currentDate.getDate()).slice(-2);
-      let month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-      let year = currentDate.getFullYear();
-      let formattedDate = `${day}-${month}-${year}`;
-      dates.push(formattedDate);
-      currentDate.setDate(currentDate.getDate() + 1);
-    }
-    return dates;
-  }
-  getDates(startDate, endDate);
-  const dates = getDates(startDate, endDate);
-  myChart.data.labels = dates;
-  myChart.update();
-	});
-
-// График
-const dates1 = Array.from({ length: 150 }, (_, i) => `День ${i + 1}`);
-
-const config = {
-  type: "bar",
-  data: {
-    labels: dates1,
-    datasets: [
-      {
-        label: "График динамики бронирования",
-        data: [
-          1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3,
-          4, 5,
-        ],
-        backgroundColor: "#02458d",
-        borderColor: "#02458d",
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    maintainAspectRatio: false,
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-    plugins: {
-      legend: { display: false, position: "bottom" },
-      title: {
-        display: false,
-        text: "Сезонность для прогнозов (рейсы Москва-Сочи)",
-      },
-    },
-  },
-};
+});
 
 // Обработчик событий на присваивание даты рейса на период вывода графика
 const date1 = document.getElementById("date");
@@ -125,6 +65,35 @@ const date2 = document.getElementById("end-date");
 const date3 = document.getElementById("start-date");
 const direction = document.getElementById("Direction");
 const flightSelection = document.getElementById("Number");
+function updateChart() {
+  let chartExists = Chart.getChart("myChart");
+  if (chartExists) {
+    chartExists.destroy();
+  }
+  const myChart = new Chart(document.getElementById("myChart"), config);
+  const chartBody = document.querySelector(".chart__body");
+  const totalLabels = myChart.data.labels.length;
+  if (totalLabels > 30) {
+    const newWidth = 1100 + (totalLabels - 30) * 40;
+    chartBody.style.width = `${newWidth}px`;
+  }
+}
+
+date2.addEventListener("change", () => {
+  let startDate = date3.value;
+  let endDate = date2.value;
+  myChart.data.labels = getDates(startDate, endDate);
+  myChart.update();
+  updateChart();
+});
+
+date3.addEventListener("change", () => {
+  let startDate = date3.value;
+  let endDate = date2.value;
+  myChart.data.labels = getDates(startDate, endDate);
+  myChart.update();
+  updateChart();
+});
 
 date1.addEventListener("input", () => {
   date2.max = date1.value;
@@ -165,6 +134,64 @@ date1.addEventListener("input", () => {
       console.log(error);
     });
 });
+
+//  Добавляем в график период для просмотра динамики бронирования
+let startDate = date3.value;
+let endDate = date2.value;
+
+function getDates(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+  const endDateTime = new Date(endDate).getTime();
+
+  while (currentDate.getTime() <= endDateTime) {
+    let day = ("0" + currentDate.getDate()).slice(-2);
+    let month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+    let year = currentDate.getFullYear();
+    let formattedDate = `${day}-${month}-${year}`;
+    dates.push(formattedDate);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+  return dates;
+}
+
+// График
+const dates1 = Array.from({ length: 150 }, (_, i) => `День ${i + 1}`);
+
+const config = {
+  type: "bar",
+  data: {
+    labels: getDates(startDate, endDate),
+    datasets: [
+      {
+        label: "График динамики бронирования",
+        data: [
+          1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3,
+          4, 5,
+        ],
+        backgroundColor: "#02458d",
+        borderColor: "#02458d",
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: { display: false, position: "bottom" },
+      title: {
+        display: false,
+        text: "Сезонность для прогнозов (рейсы Москва-Сочи)",
+      },
+    },
+  },
+};
+
 const myChart = new Chart(document.getElementById("myChart"), config);
 const chartBody = document.querySelector(".chart__body");
 const totalLabels = myChart.data.labels.length;
